885cf1a36673d268cc0287fd928c6f53ab205b41
Robert <R.Treharne@liverpool.ac.uk>
2015-05-17 16:33:15 +0100

    Chapter 7, categories page

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rango.models import Category
+from rango.models import Category, Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {'categories': category_list}
     return render(request, 'rango/index.html', context_dict)
+
+def category(request, category_name_slug):
+	context_dict = {}
+
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		context_dict['category_name'] = category.name
+
+		pages = Page.objects.filter(category=category)
+
+		context_dict['pages'] = pages
+
+		context_dict['category'] = category
+
+	except Category.DoesNotExist:
+		pass
+
+	return render(request, 'rango/category.html', context_dict)
 
 def about(request):
     context_dict = {'boldmessage': "I am bold font for the about paget"}
