9bd9af0b773664b9849ea84463b5a1260a65d4f6
Robert <R.Treharne@liverpool.ac.uk>
2015-05-17 15:21:05 +0100

    Chapter 6

--- rango/views.py
+++ rango/views.py
@@ -6,4 +6,5 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse("This is the about page.<br/><a href='/rango/'>Index</a>")
+    context_dict = {'boldmessage': "I am bold font for the about paget"}
+    return render(request, 'rango/about.html', context_dict)
